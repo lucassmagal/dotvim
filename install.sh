8a3b9d266414790b9542adb766e04ddb3ee6ebfe
Lucas Sampaio <magal.lucas@gmail.com>
2012-12-18 14:12:18 -0200

    install.sh updated, now it checks actual vim files

--- install.sh
+++ install.sh
@@ -3,7 +3,22 @@
 echo "~~ Installing .vim files ~~"
 
 if [ ! $(which git) ]; then
-echo "Git is not installed! aborting..."
+    echo "Git is not installed! Aborting..."
+    exit 1
+fi
+
+if [ ! -d "~/.vim" ]; then
+    echo "~/.vim directory already exists! Aborting..."
+    exit 1
+fi
+
+if [ ! -f "~/.vimrc" ]; then
+    echo "~/.vimrc file already exists! Aborting..."
+    exit 1
+fi
+
+if [ ! -f "~/.gvimrc" ]; then
+    echo "~/.gvimrc file already exists! Aborting..."
     exit 1
 fi
 
